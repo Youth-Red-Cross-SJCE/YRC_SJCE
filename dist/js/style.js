4f31923a2623c784652c410b28a4bf71140aae64
Andrew2432 <drew.devops@gmail.com>
2019-09-04 23:39:29 +0530

    Added carousel

--- dist/js/style.js
+++ dist/js/style.js
@@ -1,3 +1,20 @@
+onload = start;
+
+/* 
+	Autoplay slideshow in home carousel
+*/
+function start() {
+	var i = 1;
+	function Move() {
+		i = (i % 3) + 1; // 4 is the Number of image in slider
+		document.getElementById("i" + i).checked = true;
+	}
+	setInterval(Move, 3000); //change img in 3 sec
+}
+
+/* 
+	Opening and closing the side menu
+*/
 function openSlideMenu() {
 	document.getElementById("side-menu").style.width = "250px";
 	document.getElementById("main").style.marginLeft = "250px";
